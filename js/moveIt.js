bc84b4e3256ccffaaf2d41a94fd5299229af362a
karine FL <karinecci54@gmail.com>
2020-03-23 17:11:46 +0100

    redimension postit

--- js/moveIt.js
+++ js/moveIt.js
@@ -1,8 +1,10 @@
 let moveIt = false;
 let x;
 let y;
+let idMove = 0;
+var tabPostit = new Array();
 
-let monPostIt = new postIt("", 50, 50, 12);
+let monPostIt = new postIt("yellow", 50, 50, 12);
 monPostIt.afficheTest();
 
 document.addEventListener('mousemove', e => {
@@ -11,16 +13,27 @@ document.addEventListener('mousemove', e => {
 })
 document.addEventListener('mouseup', () => {
     console.log("on stop !")
+    idMove = 0;
     moveIt = false;
 })
+document.getElementById('addPostIt').addEventListener('mousedown', () => {
+    //idBouge = tabDingo.length + 1;
+    let postIt = new postIt(tabPostit.length + 1, "red", 100, 100, 12)
+    tabPostit.push(postIt);
+    //onBouge = true;
+    tabPostit[(tabPostit.length - 1)].afficheTest();
 
+});
 function refresh() {
     if (moveIt) {
+        if (moveIt && idMove != 0) {
         console.log("on déplace !")
         monPostIt.changePlace(x, y);
         monPostIt.afficheTest();
+        tabPostit[(idMove - 1)].changePlace(x, y);
+        tabPostit[(idMove - 1)].afficheTest();
     }
     setTimeout(refresh, 300)
 }
-
+}
 refresh(); 
